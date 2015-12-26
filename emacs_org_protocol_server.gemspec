00c5c93dbb9848f27de6fb31b75844065ae4a397
Tamara Temple <tamouse@gmail.com>
2015-12-26 05:55:47 -0600

    get app to work under this environment
    * Make sinatra app a module application
    * put sinatra app in library
    * call run! in exe

--- emacs_org_protocol_server.gemspec
+++ emacs_org_protocol_server.gemspec
@@ -22,5 +22,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_dependency "thin"
-  sepc.add_dependency "sinatra"
+  spec.add_dependency "sinatra"
 end
--- lib/emacs_org_protocol_server.rb
+++ lib/emacs_org_protocol_server.rb
@@ -1,5 +1,40 @@
 require "emacs_org_protocol_server/version"
+require 'sinatra/base'
+require 'uri'
 
 module EmacsOrgProtocolServer
-  # Your code goes here...
+  EMACSCLIENT=ENV['EMACSCLIENT'] || "/usr/local/bin/emacsclient"
+
+  class OPServ < Sinatra::Application
+
+    helpers do
+      def esc(s)
+        URI.escape(s, %r{[^[:alnum:]]})
+      end
+    end
+
+    get '/' do
+      if params.empty?
+        return [422, 'no params passed']
+      end
+
+      p=params['p'].to_s.downcase
+      if p.match(%r{capture})
+        template='//w'
+      else
+        template=''
+      end
+      l=params['l'].to_s
+      t=params['t'].to_s
+      s=params['s'].to_s
+
+      emacsclient_target = "org-protocol://#{p}:#{template}//#{esc(l)}/#{esc(t)}/#{esc(s)}"
+      cmd = "#{EMACSCLIENT} -n '#{emacsclient_target}'"
+      system(cmd)
+      puts "ran #{cmd}"
+      redirect to(params['l'])
+    end
+
+  end
+
 end
