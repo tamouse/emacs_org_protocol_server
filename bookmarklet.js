959ef4b0ccaf2f16796cd6caa5968103d30b5ecf
Tamara Temple <tamouse@gmail.com>
2015-12-26 05:39:26 -0600

    add js files

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:function fx(str){return encodeURIComponent(str).replace(/[!'()*]/g, function(c){return '%' + c.charCodeAt(0).toString(16);});};var captureLink=function(){var s=fx(window.getSelection().toString()),l=fx(window.location.href),t=fx(document.title),p='capture',uri='http://localhost:4567/?'+'p='+p+'&l='+l+'&t='+t+'&s='+s;window.location=uri;return uri;};captureLink();
